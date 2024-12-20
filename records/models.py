d8cc6ce065a3c48b7b44baea447fb91d73002743
oma <42239416+oma01234@users.noreply.github.com>
2024-12-20 15:30:37 +0100

    Update models.py

--- records/models.py
+++ records/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+from patients.models import Patient
 
 # this is supposed to be updated by only staff yes?
 class MedicalRecord(models.Model):
-    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='records_medical_records')
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='records_medical_records')
     diagnosis = models.TextField()
     treatment = models.TextField()
     date_recorded = models.DateField(auto_now_add=True)
@@ -13,7 +14,7 @@ class MedicalRecord(models.Model):
         return f"Medical Record for {self.patient.username} on {self.date_recorded}"
 
 class HealthReport(models.Model):
-    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='health_reports')
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='health_reports')
     report_name = models.CharField(max_length=100)
     report_file = models.FileField(upload_to='health_reports/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
@@ -22,7 +23,7 @@ class HealthReport(models.Model):
         return self.report_name
 
 class Prescription(models.Model):
-    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='records_prescriptions')
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='records_prescriptions')
     prescription_details = models.TextField()
     issued_at = models.DateField(auto_now_add=True)
     expires_at = models.DateField()
